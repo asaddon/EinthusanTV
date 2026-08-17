d8df3746a3083bb9dc9f951b7f621936ae04c98d
Asaddon <AsadThisSide@gmail.com>
2026-08-17 22:49:46 +0500

    remove redundant Stremio Central publishing logic from server startup

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 const app = require('./index.js')
-const {  publishToCentral } = require("stremio-addon-sdk");
+
 const config = require('./config.js');
 
 // create local server
@@ -9,11 +9,3 @@ app.listen((config.port), function () {
     //console.log(`HTTP addon accessible at: ${config.local}/configure`);
 });
 
-const manifest = require('./manifest.json');
-
-// Delay publishing by 20 seconds to give Render time to fully boot and route traffic
-setTimeout(() => {
-    publishToCentral("https://einthusan.asaddon.com/manifest.json")
-        .then(() => console.log(`Successfully published v${manifest.version} to Stremio Central!`))
-        .catch((e) => console.error("Central publish error (safe to ignore if already published):", e.message));
-}, 20000);
